5d6caddbdebce85aeeab4eeeffeb9ee4ed08aeca
Pasha883 <pashaboss883@gmail.com>
2023-12-11 11:49:47 +0300

    Добавлен экран интро

---
+++ core/src/com/maxadventure/game/IntroScreen.java
@@ -0,0 +1,90 @@
+package com.maxadventure.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.video.VideoPlayer;
+import com.badlogic.gdx.video.VideoPlayerCreator;
+
+import java.io.FileNotFoundException;
+
+public class IntroScreen implements Screen {
+    private  MyGdxGame myGdxGame;
+    private final SpriteBatch batch;
+    private OrthographicCamera camera;
+    private VideoPlayer videoPlayer;
+    private Texture tex;
+
+    public IntroScreen(final MyGdxGame myGdxGame, SpriteBatch batch, OrthographicCamera camera) {
+        this.batch = batch;
+        this.camera = camera;
+        this.myGdxGame = myGdxGame;
+        tex = new Texture(Gdx.files.internal("badlogic.jpg"));
+        videoPlayer = VideoPlayerCreator.createVideoPlayer();
+        videoPlayer.setOnCompletionListener(new VideoPlayer.CompletionListener() {
+            @Override
+            public void onCompletionListener(FileHandle file) {
+                myGdxGame.setScreen(myGdxGame.introScreen);
+            }
+        });
+
+    }
+
+    @Override
+    public void show() {
+        try {
+            videoPlayer.play(Gdx.files.internal("Intro.webm"));
+        } catch (FileNotFoundException e){
+            ScreenUtils.clear(1, 1, 1, 1);
+            batch.begin();
+            batch.draw(tex, 0,0);
+            batch.end();
+        }
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0,0,0,1);
+        videoPlayer.update();
+
+        batch.begin();
+        Texture frame = videoPlayer.getTexture();
+        if (frame != null){
+            batch.draw(frame,
+                    0,
+                    0,
+                    Gdx.graphics.getWidth(),
+                    Gdx.graphics.getHeight());
+        }
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
--- core/src/com/maxadventure/game/MyGdxGame.java
+++ core/src/com/maxadventure/game/MyGdxGame.java
@@ -1,27 +1,28 @@
 package com.maxadventure.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class MyGdxGame extends ApplicationAdapter {
+public class MyGdxGame extends Game {
 	SpriteBatch batch;
+	public OrthographicCamera camera;
+
+	public IntroScreen introScreen;
+
 	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		introScreen = new IntroScreen(this, batch, camera);
+		setScreen(introScreen);
 	}
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
 	
 	@Override
 	public void dispose () {
